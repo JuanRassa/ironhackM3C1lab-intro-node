15f7ff7c4fd5335c810b4f0d6bea6f67aebd7c8b
Juan Rassa <jmrassa.dev@gmail.com>
2024-01-31 21:12:38 +0000

    Done Lab.

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1 || pos <= 0) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    try {
+      console.log(Math.max(...this.items));
+      return Math.max(...this.items);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) throw new Error('Empty SortedList');
+    try {
+      console.log(Math.min(...this.items));
+      return Math.min(...this.items);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) return 0;
+    return this.items.reduce((acc, cur) => acc + cur);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error('EmptySortedList');
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
